1554898b493502a7e24844db5956a2a637365eef
Venkata Chadalawada <venchad@gmail.com>
2017-12-09 16:27:46 -0600

    Update promise-example.js

--- promise-example.js
+++ promise-example.js
@@ -25,3 +25,26 @@ asyncAdd(5,8).then((response)=>{
 },(error)=>{
   console.log('error is', error);
 });
+
+//ex3 Synchronous one after other get first result use to for second
+let SyncMulti = (a,b) => {
+  return new Promise((resolve,request) => {
+    setTimeOut(()=>{
+      if(typeOf(a)==number&&typeOf(b)==number){
+        resolve(a+b);
+      } else {
+        reject('Arguments should be numbers');
+      }
+    },2500);
+  });
+}
+
+SyncMulti(5,8).then((response)=>{
+  return SyncMulti(response,'33');
+},(error)=>{
+  console.log('error is', error);
+}).then((afterResult)=>{
+  console.log('afterResult is-',afterResult);
+}, (errorAfter)=>{
+  console.log('-----error',errorAfter);
+});
