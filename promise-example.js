e9e01f7c250f216252f7037c42f8ffe9dcb95b12
Venkata Chadalawada <venchad@gmail.com>
2017-12-09 16:18:41 -0600

    Update promise-example.js

--- promise-example.js
+++ promise-example.js
@@ -1,7 +1,27 @@
+//ex 1
 let abc = new Promise((resolve,reject) => {
   resolve('success')
   });
 
 abc.then((response)=>{
   console.log(response);
+});
+
+//ex 2
+let asyncAdd = (a,b) => {
+  return new Promise((resolve,request) => {
+    setTimeOut(()=>{
+      if(typeOf(a)==number&&typeOf(b)==number){
+        resolve(a+b);
+      } else {
+        reject('Arguments should be numbers');
+      }
+    },2500);
+  });
+}
+
+asyncAdd(5,8).then((response)=>{
+  console.log('response is',response);
+},(error)=>{
+  console.log('error is', error);
 });
