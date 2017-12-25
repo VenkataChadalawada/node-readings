2dc0712f92ca15f40b789022ae436fa3f6e96a22
Venkata Chadalawada <venchad@gmail.com>
2017-12-24 23:48:11 -0600

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,21 +1,20 @@
+const arr = [5,9,3,1,2,8,4,7,6];
 
-const arr = [4,1,2,7,5,9,0];
+console.log('---before bubble sort arr values is --', arr);
 
-for(let i=arr.length-1;i>0;i--){
-  for(let i=arr.length-1;i>0;i--){
-    console.log('----i',arr[i]);
-    if(arr[i]<arr[i-1]){
-    console.log('---before swap--',arr[i],arr[i-1]);
-      swap(i,i-1);
-     console.log('---after swap--',arr[i],arr[i-1]);
-    }
+for (let j=arr.length-1;j>=0;j--){
+  for (let i=arr.length-1;i>=0;i--){
+     if(arr[i]<arr[i-1]){
+        swap(i,i-1);
+     }
+     //by every pass you get a smaller number bubbled up
   }
 }
 
-function swap(a,b){
-var c = arr[a];
-arr[a]=arr[b];
-arr[b]=c;
-}
+console.log('---After bubble arr values is --', arr);
 
-console.log('----one time rsult',arr);
+function swap(a, b){
+let c = arr[a];
+arr[a] = arr[b];
+arr[b] = c;
+}
